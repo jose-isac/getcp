d78e7905de23a3d38b4bc00d9be81616fc307c0d
jose-isac <joseisac@tutamail.com>
2025-05-30 20:57:43 -0300

    Code that opens connection to server X.

--- main.c
+++ main.c
@@ -3,5 +3,18 @@
 
 int main(void)
 {
+    Display *display;
+    Window  *root;
+    int root_x, root_y, win_x, win_y;
+    unsigned int mask;
+
+    display = XOpenDisplay(NULL);
+    if (!display) {
+        fprintf(stderr, "Error while opening connection to server X.\n");
+        return 1;
+    }
+
+    XCloseDisplay(display);
+
     return 0;
 }
