d1f0eb78530cc4847852bd89a2fb1f293ce8c1dc
jose-isac <joseisac@tutamail.com>
2025-05-30 22:17:54 -0300

    Use err() function instead of fprintf

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 int main(void)
 {
@@ -11,8 +12,7 @@ int main(void)
 
     display = XOpenDisplay(NULL);
     if (!display) {
-        fprintf(stderr, "Error while opening connection to server X.\n");
-        return EXIT_FAILURE;
+        err(EXIT_FAILURE, "Error while opening connection to server X.\n");
     }
 
     root = DefaultRootWindow(display);
@@ -20,8 +20,7 @@ int main(void)
     if (XQueryPointer(display, root, &root, &root, &root_x, &root_y, &win_x, &win_y, &mask)) {
         fprintf(stdout, "%d,%d\n", root_x, root_y);
     } else {
-        fprintf(stderr, "Error while getting cursor position.\n");
-        return EXIT_FAILURE;
+        err(EXIT_FAILURE, "Error while getting cursor position.\n");
     }
 
     XCloseDisplay(display);
