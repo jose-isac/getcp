2f5042dc220d69a7cd7d23f1b6d64711543ff4f6
jose-isac <joseisac@tutamail.com>
2025-05-30 21:14:40 -0300

    Use the function XqueryPointer
    Also, yelds the x,y positions to stdout.

--- main.c
+++ main.c
@@ -4,13 +4,22 @@
 int main(void)
 {
     Display *display;
-    Window  *root;
+    Window   root;
     int root_x, root_y, win_x, win_y;
     unsigned int mask;
 
     display = XOpenDisplay(NULL);
     if (!display) {
         fprintf(stderr, "Error while opening connection to server X.\n");
+        return 1;
+    }
+
+    root = DefaultRootWindow(display);
+    
+    if (XQueryPointer(display, root, &root, &root, &root_x, &root_y, &win_x, &win_y, &mask)) {
+        fprintf(stdout, "%d,%d\n", root_x, root_y);
+    } else {
+        fprintf(stderr, "Error while getting cursor position.\n");
         return 1;
     }
 
